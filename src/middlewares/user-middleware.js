894b0165fdf885a75ba9f72b218b94e7a3ccffb2
Priyabrata Maji <priyabratamaji139@gmail.com>
2023-07-11 16:03:40 +0530

    Added user model and Implemented signup api

---
+++ src/middlewares/user-middleware.js
@@ -0,0 +1,27 @@
+const { StatusCodes } = require('http-status-codes');
+
+const { ErrorResponse } = require('../utils/common');
+const AppError = require('../utils/errors/app-error');
+
+function validateCreateRequest(req, res, next) {
+    if(!req.body.email) {
+        ErrorResponse.message = 'Something went wrong while creating airplane';
+        ErrorResponse.error = new AppError(['email not found in the oncoming request in the correct form'], StatusCodes.BAD_REQUEST);
+        return res
+                .status(StatusCodes.BAD_REQUEST)
+                .json(ErrorResponse);
+    }
+
+    if(!req.body.password) {
+        ErrorResponse.message = 'Something went wrong while creating airplane';
+        ErrorResponse.error = new AppError(['password not found in the oncoming request in the correct form'], StatusCodes.BAD_REQUEST);
+        return res
+                .status(StatusCodes.BAD_REQUEST)
+                .json(ErrorResponse);
+    }
+    next();
+}
+
+module.exports = {
+    validateCreateRequest
+}
---
+++ src/routes/v1/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const { UserController } = require('../../controllers');
+const { UserMiddlewares } = require('../../middlewares');
+
+// const airplaneRoutes = require('./airplane-routes');
+
+const router = express.Router();
+
+// router.use('/airplanes', airplaneRoutes);
+
+router.post('/signup',UserMiddlewares.validateCreateRequest, UserController.signup);
+
+// router.get('/info', InfoController.info);
+
+
+module.exports = router;
