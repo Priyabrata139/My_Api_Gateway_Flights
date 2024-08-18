67b750817b36425f11c7ffc720de88ecbf818766
priyabrataCee <priyabratacee@gmail.com>
2024-08-18 11:15:54 +0530

    updated publicUrls structure and authUtil isPublicRequest fuinction

--- src/utils/authUtil/authUtil.js
+++ src/utils/authUtil/authUtil.js
@@ -1,10 +1,18 @@
 const publicUrls = require("../publicUrls/publicUrls");
 
-// Function to check if the request is public
-function isPublicRequest(incomeingUrl, method) {
-  return publicUrls.some(
-    (route) => route.url === incomeingUrl && route.methods.includes(method)
-  );
+function isPublicRequest(incomingUrl, method) {
+  // Ignoring query parameters
+  const path = incomingUrl.split("?")[0];
+
+  return publicUrls.some((route) => {
+    // Check if the path matches any pattern and method in the route
+    return route.paths.some((pattern) => {
+      const regex = new RegExp(
+        `^${route.basePath}${pattern.replace(/:\w+/g, "\\d*")}$`
+      );
+      return regex.test(path) && route.methods.includes(method);
+    });
+  });
 }
 
 module.exports = {
--- src/utils/publicUrls/publicUrls.js
+++ src/utils/publicUrls/publicUrls.js
@@ -1,3 +1,5 @@
-const publicUrls = [{ url: "/api/v1/flights", methods: ["GET"] }];
+const publicUrls = [
+  { basePath: "/api/v1/flights", paths: ["", "/:id"], methods: ["GET"] },
+];
 
 module.exports = publicUrls;
